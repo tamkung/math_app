8b601ec4d5a3a3ac73e076529b0c2b62d21c1575
Pattaraphon <s6302041510087@email.kmutnb.ac.th>
2023-04-17 19:10:28 +0700

    updat page test

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:math_app/config/constant.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -12,6 +13,24 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "ประวัติ",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 30,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        toolbarHeight: 100,
+        foregroundColor: pColor,
+      ),
+      body: Container(
+        color: Colors.white,
+      ),
+    );
   }
 }
